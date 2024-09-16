6b5a44de19497f6fba9720954145f1aad01fa2ce
lucas <lucas.chorolqui@gmail.com>
2024-09-16 00:42:02 -0300

    update rows

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,68 +1,105 @@
 export default function Table() {
-  const COLUMNS = 9;
-  const ROWS = 6;
+  const COLUMNS: number = 9;
+  const ROWS: number = 6;
 
-  const range = (length: number) => Array.from({ length }, (_, i) => i);
+  const range = (length: number): number[] =>
+    Array.from({ length }, (_, i) => i);
 
-  const state = range(COLUMNS).map(() =>
-    range(ROWS).map((j) => ({ computedValue: j, value: j }))
-  );
+  let state: { computedValue: string | number; value: string | number }[][] =
+    range(COLUMNS).map(() =>
+      range(ROWS).map(() => ({ computedValue: 0, value: 0 }))
+    );
 
   // encuentra la celda mas cercana al click
-  const handleCellClick = (event: React.MouseEvent) => {
+
+  function updateCell({
+    x,
+    y,
+    value,
+  }: {
+    x: number;
+    y: number;
+    value: string | number;
+  }) {
+    const newState = structuredClone(state);
+    const cell = newState[x][y];
+    cell.computedValue = +value; // -> span
+    cell.value = value; // -> input
+
+    newState[x][y] = cell;
+
+    state = newState;
+  }
+
+  const handleCellClick = (event: React.MouseEvent): void => {
     const td = (event.target as HTMLElement).closest("td");
 
-    if (!td) return;
+    // solucion temporaria ya que fallaba al desestructurar directamente x,y desde td.dataset
+    // @ts-expect-error solucion temporaria TODO: fix types
+    const { dataset }: HTMLTableCellElement | null = td;
+    const { x, y } = dataset;
 
-    const { x, y } = td.dataset;
-
-    // const input = td.querySelector("input");
     // const span = td.querySelector("span");
+    const input = td?.querySelector("input");
 
-    console.log({ x, y });
+    // configuracion del input para que cuando se seleccione todo el texto onClick
+    input?.setSelectionRange(0, -1);
+    input?.focus();
+
+    input?.addEventListener(
+      "blur",
+      () => {
+        if (input.value === state[x][y].value) return;
+
+        updateCell({ x, y, value: input.value });
+      },
+      { once: true }
+    );
   };
 
   return (
-    <table className="border-collapse m-auto border border-[#ccc]">
-      <thead className="first:bg-slate-300">
-        <tr>
-          <th></th>
-          {range(COLUMNS).map((i) => (
-            <th
-              key={i}
-              className="text-sm h-6 font-normal border border-[#ccc]"
-            >
-              {String.fromCharCode(65 + i)}
-            </th>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody onClick={(e) => handleCellClick(e)}>
-        {range(ROWS).map((row) => (
-          <tr key={row} className="border border-[#ccc]">
-            <th className="bg-slate-300 font-normal text-sm w-[64px] text-center relative">
-              {row + 1}
-            </th>
-            {range(COLUMNS).map((column) => (
-              <td
-                key={column}
-                data-x={column}
-                data-y={row}
-                className="fist:bg-red-400 relative"
+    <div className="w-full overflow-x-auto">
+      <table className="w-full border-collapse border border-[#ccc]">
+        <thead>
+          <tr className="bg-slate-300">
+            <th className="w-[64px] border border-[#ccc]"></th>
+            {range(COLUMNS).map((i) => (
+              <th
+                key={i}
+                className="text-sm h-6 font-normal border border-[#ccc] w-[120px]"
               >
-                <span className="w-full absolute align-middle inline-flex justify-center items-center inset-0">
-                  {state[column][row].computedValue}
-                </span>
-                <input
-                  className="w-full relative flex z-10 justify-center items-center inset-0 border opacity-0 pointer-events-none focus:outline-sky-500"
-                  value={state[column][row].value}
-                />
-              </td>
+                {String.fromCharCode(65 + i)}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody onClick={(e) => handleCellClick(e)}>
+          {range(ROWS).map((row) => (
+            <tr key={row}>
+              <th className="bg-slate-300 font-normal text-sm w-[64px] text-center border border-[#ccc]">
+                {row + 1}
+              </th>
+              {range(COLUMNS).map((column) => (
+                <td
+                  key={column}
+                  data-x={column}
+                  data-y={row}
+                  className="relative border border-[#ccc] p-0 h-8"
+                >
+                  <span className="absolute inset-0 flex bg-slate-100 items-center justify-center pointer-events-none">
+                    {state[column][row].computedValue}
+                  </span>
+                  <input
+                    className="absolute inset-0 w-full h-full px-1 opacity-0 focus:outline-none focus:ring-2 focus:opacity-100 focus:ring-cyan-600 focus:border-transparent"
+                    // value={state[column][row].value}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
